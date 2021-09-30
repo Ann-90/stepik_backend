cfd8b00877bf10c86c1e3a3e5038dd4bb850ca94
Ann Vorobyeva <ann@fastlaw.io>
2021-09-30 22:36:57 +0300

    1

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
-#!/git/bash -x
-
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+#!/bin/bash
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
+#sudo /etc/init.d/mysql start
