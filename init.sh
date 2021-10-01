6140d985a79df1ac9c90c3eb25b13c566fbc17d6
Ann Vorobyeva <ann@fastlaw.io>
2021-10-01 13:45:12 +0300

    hide config http

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo rm /etc/nginx/sites-enabled/test.conf
+sudo rm -rf /etc/nginx/sites-enabled/* 
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
